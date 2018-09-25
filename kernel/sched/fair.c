bf4fb0ff2e9c68f1b15fa7086c0974810d654bbe
Peter Zijlstra <peterz@infradead.org>
2018-09-25 15:58:20 +0200

    sched: Split scheduler and execution contexts
    Let's define the scheduling context as all the scheduler state
    in task_struct for the task selected to run, and the execution
    context as all state required to actually run the task.
    
    Currently both are intertwined in task_struct. We want to
    logically split these such that we can use the scheduling
    context of the task selected to be scheduled, but use the
    execution context of a different task to actually be run.
    
    To this purpose, introduce rq_selected() macro to point to the
    task_struct selected from the runqueue by the scheduler, and
    will be used for scheduler state, and preserve rq->curr to
    indicate the execution context of the task that will actually be
    run.
    
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Youssef Esmat <youssefesmat@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: kernel-team@android.com
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Tested-by: Metin Kaya <metin.kaya@arm.com>
    Reviewed-by: Metin Kaya <metin.kaya@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20181009092434.26221-5-juri.lelli@redhat.com
    [add additional comments and update more sched_class code to use
     rq::proxy]
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: Rebased and resolved minor collisions, reworked to use
     accessors, tweaked update_curr_common to use rq_proxy fixing rt
     scheduling issues]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v2:
    * Reworked to use accessors
    * Fixed update_curr_common to use proxy instead of curr
    v3:
    * Tweaked wrapper names
    * Swapped proxy for selected for clarity
    v4:
    * Minor variable name tweaks for readability
    * Use a macro instead of a inline function and drop
      other helper functions as suggested by Peter.
    * Remove verbose comments/questions to avoid review
      distractions, as suggested by Dietmar
    v5:
    * Add CONFIG_PROXY_EXEC option to this patch so the
      new logic can be tested with this change
    * Minor fix to grab rq_selected when holding the rq lock
    v7:
    * Minor spelling fix and unused argument fixes suggested by
      Metin Kaya
    * Switch to curr_selected for consistency, and minor rewording
      of commit message for clarity
    * Rename variables selected instead of curr when we're using
      rq_selected()
    * Reduce macros in CONFIG_SCHED_PROXY_EXEC ifdef sections,
      as suggested by Metin Kaya
    v8:
    * Use rq->curr, not rq_selected with task_tick, as suggested by
      Valentin
    * Minor rework to reorder this with CONFIG_SCHED_PROXY_EXEC patch
    v10:
    * Use rq_selected in push_rt_task & get_push_task
    v11:
    * Rework to use selected instead of curr in a few cases we were
      previously assigning curr = rq_selected() to minimize lines of
      change. Suggested by Metin.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1172,15 +1172,15 @@ static s64 update_curr_se(struct rq *rq, struct sched_entity *curr)
  */
 s64 update_curr_common(struct rq *rq)
 {
-	struct task_struct *curr = rq->curr;
+	struct task_struct *selected = rq_selected(rq);
 	s64 delta_exec;
 
-	delta_exec = update_curr_se(rq, &curr->se);
+	delta_exec = update_curr_se(rq, &selected->se);
 	if (unlikely(delta_exec <= 0))
 		return delta_exec;
 
-	account_group_exec_runtime(curr, delta_exec);
-	cgroup_account_cputime(curr, delta_exec);
+	account_group_exec_runtime(selected, delta_exec);
+	cgroup_account_cputime(selected, delta_exec);
 
 	return delta_exec;
 }
@@ -1218,7 +1218,7 @@ static void update_curr(struct cfs_rq *cfs_rq)
 
 static void update_curr_fair(struct rq *rq)
 {
-	update_curr(cfs_rq_of(&rq->curr->se));
+	update_curr(cfs_rq_of(&rq_selected(rq)->se));
 }
 
 static inline void
@@ -6461,7 +6461,7 @@ static void hrtick_start_fair(struct rq *rq, struct task_struct *p)
 		s64 delta = slice - ran;
 
 		if (delta < 0) {
-			if (task_current(rq, p))
+			if (task_current_selected(rq, p))
 				resched_curr(rq);
 			return;
 		}
@@ -6476,12 +6476,12 @@ static void hrtick_start_fair(struct rq *rq, struct task_struct *p)
  */
 static void hrtick_update(struct rq *rq)
 {
-	struct task_struct *curr = rq->curr;
+	struct task_struct *selected = rq_selected(rq);
 
-	if (!hrtick_enabled_fair(rq) || curr->sched_class != &fair_sched_class)
+	if (!hrtick_enabled_fair(rq) || selected->sched_class != &fair_sched_class)
 		return;
 
-	hrtick_start_fair(rq, curr);
+	hrtick_start_fair(rq, selected);
 }
 #else /* !CONFIG_SCHED_HRTICK */
 static inline void
@@ -8082,9 +8082,9 @@ static void set_next_buddy(struct sched_entity *se)
  */
 static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_flags)
 {
-	struct task_struct *curr = rq->curr;
-	struct sched_entity *se = &curr->se, *pse = &p->se;
-	struct cfs_rq *cfs_rq = task_cfs_rq(curr);
+	struct task_struct *selected = rq_selected(rq);
+	struct sched_entity *se = &selected->se, *pse = &p->se;
+	struct cfs_rq *cfs_rq = task_cfs_rq(selected);
 	int next_buddy_marked = 0;
 	int cse_is_idle, pse_is_idle;
 
@@ -8115,11 +8115,11 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 	 * prevents us from potentially nominating it as a false LAST_BUDDY
 	 * below.
 	 */
-	if (test_tsk_need_resched(curr))
+	if (test_tsk_need_resched(rq->curr))
 		return;
 
 	/* Idle tasks are by definition preempted by non-idle tasks. */
-	if (unlikely(task_has_idle_policy(curr)) &&
+	if (unlikely(task_has_idle_policy(selected)) &&
 	    likely(!task_has_idle_policy(p)))
 		goto preempt;
 
@@ -9099,7 +9099,7 @@ static bool __update_blocked_others(struct rq *rq, bool *done)
 	 * update_load_avg() can call cpufreq_update_util(). Make sure that RT,
 	 * DL and IRQ signals have been updated before updating CFS.
 	 */
-	curr_class = rq->curr->sched_class;
+	curr_class = rq_selected(rq)->sched_class;
 
 	thermal_pressure = arch_scale_thermal_pressure(cpu_of(rq));
 
@@ -12471,7 +12471,7 @@ prio_changed_fair(struct rq *rq, struct task_struct *p, int oldprio)
 	 * our priority decreased, or if we are not currently running on
 	 * this runqueue and our priority is higher than the current's
 	 */
-	if (task_current(rq, p)) {
+	if (task_current_selected(rq, p)) {
 		if (p->prio > oldprio)
 			resched_curr(rq);
 	} else
@@ -12574,7 +12574,7 @@ static void switched_to_fair(struct rq *rq, struct task_struct *p)
 		 * kick off the schedule if running, otherwise just see
 		 * if we can still preempt the current task.
 		 */
-		if (task_current(rq, p))
+		if (task_current_selected(rq, p))
 			resched_curr(rq);
 		else
 			check_preempt_curr(rq, p, 0);
