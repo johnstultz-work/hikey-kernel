532b06824c9afc3972ea5b43b2e46d7966729921
Valentin Schneider <valentin.schneider@arm.com>
2020-06-24 14:18:51 +0100

    FOLDDOWN: locking/mutex: Add p->blocked_on wrappers for correctness checks
    This lets us assert p->blocked_lock is held whenever we access
    p->blocked_on, as well as warn us for unexpected state changes.
    
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Youssef Esmat <youssefesmat@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: kernel-team@android.com
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    [fix conflicts, call in more places]
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: tweaked commit subject, added get_task_blocked_on() as well]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v2:
    * Added get_task_blocked_on() accessor
    v4:
    * Address READ_ONCE usage that was dropped in v2
    * Reordered to be a later add on to the main patch series as
      Peter was unhappy with similar wrappers in other patches.
    v5:
    * Added some extra correctness checking in wrappers
    v7:
    * Tweaks to reorder this change in the patch series
    * Minor cleanup to set_task_blocked_on() suggested by Metin Kaya

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2263,6 +2263,33 @@ static inline int rwlock_needbreak(rwlock_t *lock)
 #endif
 }
 
+static inline void set_task_blocked_on(struct task_struct *p, struct mutex *m)
+{
+	lockdep_assert_held(&p->blocked_lock);
+
+	/*
+	 * Check we are clearing values to NULL or setting NULL
+	 * to values to ensure we don't overwrite exisiting mutex
+	 * values or clear already cleared values
+	 */
+	WARN_ON((!m && !p->blocked_on) || (m && p->blocked_on));
+
+	p->blocked_on = m;
+	p->blocked_on_state = m ? BO_BLOCKED : BO_RUNNABLE;
+}
+
+static inline struct mutex *get_task_blocked_on(struct task_struct *p)
+{
+	lockdep_assert_held(&p->blocked_lock);
+
+	return p->blocked_on;
+}
+
+static inline struct mutex *get_task_blocked_on_once(struct task_struct *p)
+{
+	return READ_ONCE(p->blocked_on);
+}
+
 static __always_inline bool need_resched(void)
 {
 	return unlikely(tif_need_resched());
