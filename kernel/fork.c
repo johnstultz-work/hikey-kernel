d1d1a68a9155b5e0f9e482cd83ad455462e29eaa
John Stultz <jstultz@google.com>
2023-07-14 04:57:30 +0000

    sched: Add blocked owner handling to proxy()
    Adds a simple implementation of blocked owner handling
    where we simply queue the selected task on the blocked
    (sleeping) owner task and deactivate it, re-activating
    it later when the blocked owner is woken up.
    
    This was broken out from the larger proxy() patch
    
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -2464,6 +2464,7 @@ __latent_entropy struct task_struct *copy_process(
 
 	p->blocked_on = NULL; /* not blocked yet */
 	p->blocked_on_waking = false; /* not blocked yet */
+	INIT_LIST_HEAD(&p->blocked_entry);
 
 #ifdef CONFIG_BCACHE
 	p->sequential_io	= 0;
