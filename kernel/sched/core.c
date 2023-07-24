907b073055fdb9d2f9e78d0a192cf2000542d031
John Stultz <jstultz@google.com>
2023-07-24 20:42:56 +0000

    sched: Add logic for preserve resched
    If we need to do something with the previously running task
    we switch to idle first. However, we want to make sure we
    don't clear the needs_resched bit so that we will quickly
    return to __schedule to pick again after the context switch
    
    Signed-off-by: John Stultz <jstultz@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6701,6 +6701,7 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 	struct rq *rq;
 	bool proxied;
 	int cpu;
+	bool preserve_need_resched = false;
 
 	cpu = smp_processor_id();
 	rq = cpu_rq(cpu);
@@ -6760,9 +6761,12 @@ pick_again:
 			rq_repin_lock(rq, &rf);
 			goto pick_again;
 		}
+		if (next == rq->idle && prev == rq->idle)
+			preserve_need_resched = true;
 	}
 
-	clear_tsk_need_resched(prev);
+	if (!preserve_need_resched)
+		clear_tsk_need_resched(prev);
 	clear_preempt_need_resched();
 #ifdef CONFIG_SCHED_DEBUG
 	rq->last_seen_need_resched_ns = 0;
