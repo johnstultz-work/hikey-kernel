745799918be7b43bdc2ab27b1159b045c0a4c14e
Peter Zijlstra <peterz@infradead.org>
2023-07-24 20:42:56 +0000

    sched: Add logic for preserve resched
    If in proxy() we need to do something with the previously
    running task we switch to idle first. However, we want to make
    sure we don't clear the needs_resched bit so that we will
    quickly return to __schedule to pick again after the context
    switch
    
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Youssef Esmat <youssefesmat@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E . McKenney" <paulmck@kernel.org>
    Cc: kernel-team@android.com
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Juri Lelli <juri.lelli@redhat.com>
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: This patch was split out from the larger proxy patch]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v5:
    * Split out from larger proxy patch

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -6746,6 +6746,7 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 	struct rq *rq;
 	bool proxied;
 	int cpu;
+	bool preserve_need_resched = false;
 
 	cpu = smp_processor_id();
 	rq = cpu_rq(cpu);
@@ -6806,9 +6807,12 @@ pick_again:
 			rq_repin_lock(rq, &rf);
 			goto pick_again;
 		}
+		if (next == rq->idle && prev == rq->idle)
+			preserve_need_resched = true;
 	}
 
-	clear_tsk_need_resched(prev);
+	if (!preserve_need_resched)
+		clear_tsk_need_resched(prev);
 	clear_preempt_need_resched();
 #ifdef CONFIG_SCHED_DEBUG
 	rq->last_seen_need_resched_ns = 0;
