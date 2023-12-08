13e276358fa1e964de9ba235a32921fa13409c73
Connor O'Brien <connoro@google.com>
2023-12-08 10:36:57 -0800

    sched: Add move_queued_task_locked helper
    Switch logic that deactivates, sets the task cpu,
    and reactivates a task on a different rq to use a
    helper that will be later extended to push entire
    blocked task chains.
    
    This patch was broken out from a larger chain migration
    patch originally by Connor O'Brien.
    
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Youssef Esmat <youssefesmat@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: kernel-team@android.com
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Tested-by: Metin Kaya <metin.kaya@arm.com>
    Reviewed-by: Metin Kaya <metin.kaya@arm.com>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Reviewed-by: Qais Yousef <qyousef@layalina.io>
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: split out from larger chain migration patch]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v8:
    * Renamed from push_task_chain to do_push_task so it makes
      more sense without proxy-execution
    v10:
    * Changed name to move_queued_task_locked as suggested by Valentin
    v11:
    * Also use new helper in __migrate_swap_task() and
      try_steal_cookie() as suggested by Qais Yousef

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3519,5 +3519,16 @@ static inline void init_sched_mm_cid(struct task_struct *t) { }
 
 extern u64 avg_vruntime(struct cfs_rq *cfs_rq);
 extern int entity_eligible(struct cfs_rq *cfs_rq, struct sched_entity *se);
+#ifdef CONFIG_SMP
+static inline
+void move_queued_task_locked(struct rq *rq, struct rq *dst_rq, struct task_struct *task)
+{
+	lockdep_assert_rq_held(rq);
+	lockdep_assert_rq_held(dst_rq);
+	deactivate_task(rq, task, 0);
+	set_task_cpu(task, dst_rq->cpu);
+	activate_task(dst_rq, task, 0);
+}
+#endif
 
 #endif /* _KERNEL_SCHED_SCHED_H */
