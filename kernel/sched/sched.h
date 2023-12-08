2b89767a0ae736d4a12f34f57d3fe3d2c72f5666
Connor O'Brien <connoro@google.com>
2023-12-08 10:51:05 -0800

    sched: Consolidate pick_*_task to task_is_pushable helper
    This patch consolidates rt and deadline pick_*_task functions to
    a task_is_pushable() helper
    
    This patch was broken out from a larger chain migration
    patch originally by Connor O'Brien.
    
    Cc: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@redhat.com>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
    Cc: Valentin Schneider <vschneid@redhat.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Ben Segall <bsegall@google.com>
    Cc: Zimuzo Ezeozue <zezeozue@google.com>
    Cc: Youssef Esmat <youssefesmat@google.com>
    Cc: Mel Gorman <mgorman@suse.de>
    Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: "Paul E. McKenney" <paulmck@kernel.org>
    Cc: Metin Kaya <Metin.Kaya@arm.com>
    Cc: Xuewen Yan <xuewen.yan94@gmail.com>
    Cc: K Prateek Nayak <kprateek.nayak@amd.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: kernel-team@android.com
    Tested-by: K Prateek Nayak <kprateek.nayak@amd.com>
    Tested-by: Metin Kaya <metin.kaya@arm.com>
    Reviewed-by: Metin Kaya <metin.kaya@arm.com>
    Reviewed-by: Valentin Schneider <vschneid@redhat.com>
    Signed-off-by: Connor O'Brien <connoro@google.com>
    [jstultz: split out from larger chain migration patch,
     renamed helper function]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v7:
    * Split from chain migration patch
    * Renamed function

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -3529,6 +3529,16 @@ void move_queued_task_locked(struct rq *rq, struct rq *dst_rq, struct task_struc
 	set_task_cpu(task, dst_rq->cpu);
 	activate_task(dst_rq, task, 0);
 }
+
+static inline
+int task_is_pushable(struct rq *rq, struct task_struct *p, int cpu)
+{
+	if (!task_on_cpu(rq, p) &&
+	    cpumask_test_cpu(cpu, &p->cpus_mask))
+		return 1;
+
+	return 0;
+}
 #endif
 
 #endif /* _KERNEL_SCHED_SCHED_H */
