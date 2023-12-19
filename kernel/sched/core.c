BayLibre/android_kernel_common

FIX: Old special logic in scheduler_tick()
This is an old fix from Connor's tree - need to validate if this is really needed

Cc: Joel Fernandes <joelaf@google.com>
Cc: Qais Yousef <qyousef@google.com>
Cc: Ingo Molnar <mingo@redhat.com>
Cc: Peter Zijlstra <peterz@infradead.org>
Cc: Juri Lelli <juri.lelli@redhat.com>
Cc: Vincent Guittot <vincent.guittot@linaro.org>
Cc: Dietmar Eggemann <dietmar.eggemann@arm.com>
Cc: Valentin Schneider <vschneid@redhat.com>
Cc: Steven Rostedt <rostedt@goodmis.org>
Cc: Ben Segall <bsegall@google.com>
Cc: Zimuzo Ezeozue <zezeozue@google.com>
Cc: Youssef Esmat <youssefesmat@google.com>
Cc: Mel Gorman <mgorman@suse.de>
Cc: Daniel Bristot de Oliveira <bristot@redhat.com>
Cc: Will Deacon <will@kernel.org>
Cc: Waiman Long <longman@redhat.com>
Cc: Boqun Feng <boqun.feng@gmail.com>
Cc: "Paul E . McKenney" <paulmck@kernel.org>
Cc: kernel-team@android.com
Change-Id: I310b6d0f0754f779c09d01fe43efc2d2eb93a89d
Signed-off-by: John Stultz <jstultz@google.com>

kernel/sched/core.c
@@ -6016,6 +6016,15 @@ void scheduler_tick(void)
 	rq_lock(rq, &rf);
 	curr = rq_selected(rq);
 
+#ifdef CONFIG_PROXY_EXEC
+	if (task_cpu(curr) != cpu) {
+		BUG_ON(!test_preempt_need_resched() &&
+		       !tif_need_resched());
+		rq_unlock(rq, &rf);
+		return;
+	}
+#endif
+
 	update_rq_clock(rq);
 
 	thermal_pressure = arch_scale_thermal_pressure(cpu_of(rq));

johnstultz-work/hikey-kernel

Cc: Metin Kaya <Metin.Kaya@arm.com>
Cc: Thomas Gleixner <tglx@linutronix.de>

kernel/sched/core.c
@@ -5986,6 +5986,15 @@ void scheduler_tick(void)
 	rq_lock(rq, &rf);
 	selected = rq_selected(rq);
 
+#ifdef CONFIG_SCHED_PROXY_EXEC
+	if (task_cpu(selected) != cpu) {
+		BUG_ON(!test_preempt_need_resched() &&
+		       !tif_need_resched());
+		rq_unlock(rq, &rf);
+		return;
+	}
+#endif
+
 	update_rq_clock(rq);
 	thermal_pressure = arch_scale_thermal_pressure(cpu_of(rq));
 	update_thermal_load_avg(rq_clock_thermal(rq), rq, thermal_pressure);