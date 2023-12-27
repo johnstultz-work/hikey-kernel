e42f57cf50ffb1fbcf28bbd3b5f2bc7aadead018
Metin Kaya <metin.kaya@arm.com>
2023-12-27 12:09:08 +0000

    sched: Add trace events for proxy execution
    Add sched_[start, finish]_task_selection trace events to measure the
    latency of proxy execution patches in task selection.
    
    Moreover, introduce trace events for interesting events in proxy
    execution:
    1. sched_pe_enqueue_sleeping_task: a task gets enqueued on wait queue of
       a sleeping task (mutex owner).
    2. sched_pe_activate_blocked_entity: When owner is woken and we activate
       tasks enqueued on the owner' blocked entities list.
    3. sched_pe_migration: dependency chain crosses remote CPU.
    4. sched_pe_return_migration: returning a previously proxy-migrated task
       to it's originating cpu.
    5. sched_pe_owner_is_migrating: mutex owner task is mid-migration.
    New trace events can be tested via this command:
    $ perf trace -e sched:sched_start_task_selection \
      -e sched:sched_finish_task_selection \
      -e sched:sched_pe_enqueue_sleeping_task \
      -e sched:sched_pe_activate_blocked_entity \
      -e sched:sched_pe_migration \
      -e sched:sched_pe_return_migration \
      -e sched:sched_pe_owner_is_migrating
    
    Note: These trace events are not intended to merge upstream. Instead,
    they are only for making PE tests easier and will be converted to trace
    points once Proxy Execution patches hit upstream.
    
    CC: John Stultz <jstultz@google.com>
    CC: Joel Fernandes <joelaf@google.com>
    Cc: Qais Yousef <qyousef@layalina.io>
    CC: Ingo Molnar <mingo@redhat.com>
    CC: Peter Zijlstra <peterz@infradead.org>
    CC: Juri Lelli <juri.lelli@redhat.com>
    CC: Vincent Guittot <vincent.guittot@linaro.org>
    CC: Dietmar Eggemann <dietmar.eggemann@arm.com>
    CC: Valentin Schneider <vschneid@redhat.com>
    CC: Steven Rostedt <rostedt@goodmis.org>
    CC: Masami Hiramatsu <mhiramat@kernel.org>
    CC: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    CC: Ben Segall <bsegall@google.com>
    CC: Zimuzo Ezeozue <zezeozue@google.com>
    CC: Youssef Esmat <youssefesmat@google.com>
    CC: Mel Gorman <mgorman@suse.de>
    CC: Will Deacon <will@kernel.org>
    CC: Waiman Long <longman@redhat.com>
    CC: Boqun Feng <boqun.feng@gmail.com>
    CC: "Paul E. McKenney" <paulmck@kernel.org>
    CC: Xuewen Yan <xuewen.yan94@gmail.com>
    CC: K Prateek Nayak <kprateek.nayak@amd.com>
    CC: Thomas Gleixner <tglx@linutronix.de>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    CC: kernel-team@android.com
    CC: linux-trace-kernel@vger.kernel.org
    Signed-off-by: Metin Kaya <metin.kaya@arm.com>
    [jstultz: Add more details to the traceevents for validating
     proxy behavior]
    Signed-off-by: John Stultz <jstultz@google.com>
    ---
    v10:
    * Add more details to the traceevents (owner task for proxy
      migrations, and  prev, selected and next for task selection)
      so its easier to understand the proxy behavior.
    v11:
    * Rename sched_pe_owner_is_migrating and include owner & waiter
      in the trace_event
    * Add trace-event for activation of blocked entities
    * Rename sched_pe_migration
    * Add sched_pe_return_migration to track return migration events

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3977,6 +3977,7 @@ static void activate_blocked_waiters(struct rq *target_rq,
 			raw_spin_unlock_irqrestore(&owner->blocked_lock, flags);
 
 			do_activate_blocked_waiter(target_rq, p, en_flags);
+			trace_sched_pe_activate_blocked_entity(owner, p);
 
 			raw_spin_lock_irqsave(&p->blocked_lock, flags);
 			if (list_empty(&p->blocked_activation_node)) {
@@ -4130,8 +4131,10 @@ static int ttwu_runnable(struct task_struct *p, int wake_flags)
 			update_rq_clock(rq);
 			wakeup_preempt(rq, p, wake_flags);
 		}
-		if (proxy_needs_return(rq, p))
+		if (proxy_needs_return(rq, p)) {
+			trace_sched_pe_return_migration(p, p->wake_cpu);
 			goto out;
+		}
 		ttwu_do_wakeup(p);
 		ret = 1;
 	}
@@ -7044,6 +7047,7 @@ static void proxy_enqueue_on_owner(struct rq *rq, struct task_struct *owner,
 	 */
 	if (!owner->on_rq) {
 		WARN_ON(!next->on_rq);
+		trace_sched_pe_enqueue_sleeping_task(owner, next);
 
 		deactivate_task(rq, next, DEQUEUE_SLEEP);
 		/*
@@ -7149,12 +7153,15 @@ find_proxy_task(struct rq *rq, struct task_struct *next, struct rq_flags *rf)
 			if (curr_in_chain)
 				return proxy_resched_idle(rq, next);
 			WARN_ON(!is_cpu_allowed(p, p->wake_cpu));
+			trace_sched_pe_return_migration(p, p->wake_cpu);
 			proxy_migrate_task(rq, rf, p, p->wake_cpu);
 			return NULL;
 		}
 
 		owner_cpu = task_cpu(owner);
 		if (owner_cpu != cur_cpu) {
+			trace_sched_pe_migration(next, owner);
+
 			/*
 			 * @owner can disappear, simply migrate to @owner_cpu and leave that CPU
 			 * to sort things out.
@@ -7169,6 +7176,8 @@ find_proxy_task(struct rq *rq, struct task_struct *next, struct rq_flags *rf)
 		}
 
 		if (task_on_rq_migrating(owner)) {
+			trace_sched_pe_owner_is_migrating(owner, p);
+
 			/*
 			 * One of the chain of mutex owners is currently migrating to this
 			 * CPU, but has not yet been enqueued because we are holding the
@@ -7396,6 +7405,8 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 	}
 
 	prev_not_proxied = !prev->blocked_donor;
+
+	trace_sched_start_task_selection(prev, cpu, task_is_blocked(prev));
 pick_again:
 	next = pick_next_task(rq, rq_selected(rq), &rf);
 	rq_set_selected(rq, next);
@@ -7410,6 +7421,7 @@ pick_again:
 		if (next == rq->idle)
 			preserve_need_resched = true;
 	}
+	trace_sched_finish_task_selection(rq_selected(rq), next, cpu);
 
 	if (!preserve_need_resched)
 		clear_tsk_need_resched(prev);
